cff6d15d623ac4306d9993626142a49d4fb18271
Rajesh Sharma <rajesh@surf.net.np>
2013-08-08 17:09:05 -0700

    adding logging support

--- app.rb
+++ app.rb
@@ -3,7 +3,22 @@ require 'sinatra'
 require_relative 'lib/broker'
 require_relative 'lib/commands'
 
+configure do
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
 post '/' do
-  Broker.deploy(request.body.read.to_s)
+  begin
+    request_body = request.body.read.to_s
+    logger.info "===Request Received==="
+    logger.info request_body
+    logger.info "===Request END==="
+    Broker.deploy(request_body)
+  rescue Exception => e
+    logger.info "===Something Went Wrong==="
+    logger.info e
+  end
   ""
 end
--- config.ru
+++ config.ru
@@ -2,4 +2,11 @@ require 'rubygems'
 require 'sinatra'
 require File.expand_path '../app.rb', __FILE__
 
+log = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+$stdout.reopen(log)
+$stderr.reopen(log)
+
+$stdout.sync = true
+$stderr.sync = true
+
 run Sinatra::Application
